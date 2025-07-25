8574b6cfc20b3f95474edd6551a07944a782ed36
Itamar Reif <9663129+itamarreif@users.noreply.github.com>
2025-07-25 18:11:21 -0400

    feat(ui): basic ui (#18)
    basic ui with shadcn. contains tables for spot prices and generated signals

---
+++ webapp/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "kuma",
+  description: "Cross-chain arbitrage bot for Tycho Community Extensions TAP-6",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
