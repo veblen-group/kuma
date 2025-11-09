fe6bfd1fafea76cc51a71b9e07e583b3ef307c39
Itamar Reif <itamar.reif@gmail.com>
2025-11-09 18:14:33 -0500

    change env var name and add conditional filter for verbose

--- crates/kumad/src/telemetry.rs
+++ crates/kumad/src/telemetry.rs
@@ -1,14 +1,14 @@
-use std::{env, sync::OnceLock};
+use std::{env, ops::Not, sync::OnceLock};
 
 use tracing::{Subscriber, level_filters::LevelFilter};
 use tracing_error::ErrorLayer;
-use tracing_subscriber::{EnvFilter, Layer, fmt, layer::SubscriberExt as _};
+use tracing_subscriber::{EnvFilter, Layer, filter::filter_fn, fmt, layer::SubscriberExt as _};
 
 static TELEMETRY_INIT: OnceLock<()> = OnceLock::new();
 
 pub fn get_subscriber() -> impl Subscriber + Send + Sync {
     // use the passed log level or default to RUST_LOG value
-    let filter = EnvFilter::from_default_env()
+    let directives = EnvFilter::from_default_env()
         .add_directive("h2=warn".parse().expect("well-formed"))
         .add_directive(
             "hyper_util=warn"
@@ -46,35 +46,53 @@ pub fn get_subscriber() -> impl Subscriber + Send + Sync {
                 .expect("well-formed tracing directive"),
         );
 
-    let concise = fmt::layer()
-        .with_file(false)
-        .with_target(false)
-        .with_line_number(false)
-        .with_level(true)
-        .compact()
-        .with_writer(std::io::stdout)
-        .with_filter(LevelFilter::INFO);
-
-    let is_verbose = env::var("RUST_VERBOSE")
+    let is_split_view = env::var("KUMA_SPLIT_VIEW")
         .map(|val| val == "1" || val.to_lowercase() == "true")
         .unwrap_or(false);
 
-    let verbose = is_verbose.then_some(
+    let verbose = is_split_view.then_some(
         fmt::layer()
             // .with_file(true)
             // .with_line_number(true)
             .with_target(false)
             .with_level(true)
             .with_writer(std::io::stderr)
-            // .with_filter(LevelFilter::TRACE.and(LevelFilter::INFO.not())),
-            .compact(),
+            .compact()
+            .with_filter(filter_fn(|metadata| {
+                if metadata.is_span() {
+                    return true;
+                }
+                *metadata.level() != tracing::Level::INFO
+            })),
+    );
+
+    let concise = is_split_view.then_some(
+        fmt::layer()
+            .with_file(false)
+            .with_target(false)
+            .with_line_number(false)
+            .with_level(true)
+            .compact()
+            .with_writer(std::io::stdout)
+            .with_filter(LevelFilter::INFO),
+    );
+
+    let non_split_fmt = is_split_view.not().then_some(
+        fmt::layer()
+            .with_file(false)
+            .with_target(false)
+            .with_line_number(false)
+            .with_level(true)
+            .compact()
+            .with_filter(EnvFilter::from_default_env()),
     );
 
     tracing_subscriber::Registry::default()
-        .with(filter)
+        .with(directives)
         .with(ErrorLayer::default())
         .with(verbose)
         .with(concise)
+        .with(non_split_fmt)
 }
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
