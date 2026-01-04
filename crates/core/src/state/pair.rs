c10c825b08970faffbfb2e2e5f8ddbb1b7b380ae
Itamar Reif <itamar.reif@gmail.com>
2026-01-04 14:40:32 -0500

    undo zero2one

--- crates/core/src/state/pair.rs
+++ crates/core/src/state/pair.rs
@@ -14,38 +14,27 @@ use tycho_simulation::{
 /// Represents a pair of tokens, normalized to Uniswap's zero2one direction.
 /// TODO: it isnt normalized to zero2one, its normalized by symbol order
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct Pair {
-    pub token_a: Token,
-    pub token_b: Token,
-}
+pub struct Pair(Token, Token);
 
 impl Pair {
     pub fn new(maybe_a: Token, maybe_b: Token) -> Self {
         if maybe_a.symbol < maybe_b.symbol {
-            Self {
-                token_a: maybe_a,
-                token_b: maybe_b,
-            }
+            Self(maybe_a, maybe_b)
         } else {
-            Self {
-                token_b: maybe_b,
-                token_a: maybe_a,
-            }
+            Self(maybe_b, maybe_a)
         }
     }
 
     pub fn in_token_vec(&self, tokens: &[Token]) -> bool {
-        tokens.contains(&self.token_a) && tokens.contains(&self.token_b)
+        tokens.contains(&self.0) && tokens.contains(&self.1)
     }
 
     pub fn token_a(&self) -> &Token {
-        // TODO: zero2one token 0
-        &self.token_a
+        &self.0
     }
 
     pub fn token_b(&self) -> &Token {
-        // TODO: zero2one token 0
-        &self.token_b
+        &self.1
     }
 
     pub fn zero2one(token_a: Token, token_b: Token) -> (Token, Token) {
@@ -62,9 +51,8 @@ impl Display for Pair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{}-{}",             // ({}, {})",
-            self.token_a.symbol, // self.token_a.address,
-            self.token_b.symbol, //self.token_b.address
+            "{}-{}", // ({}, {})",
+            self.0.symbol, self.1.symbol,
         )
     }
 }
